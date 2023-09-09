12896fab00b41f17bd3eb1a70449271c59858900
Yusuke Kawamoto <novogrammer@gmail.com>
2023-09-10 01:07:00 +0900

    mpa設定が必要だった

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,13 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 export default defineConfig({
   base:"/hello-opencv-filter-browser/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        main_canny: resolve(__dirname, 'canny/index.html'),
+      },
+    },
+  },
 })
